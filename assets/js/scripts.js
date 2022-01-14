6a5f3b715ab6869a620af1664b8391158d78dc99
Phil Buchanan <info@philbuchanan.com>
2022-01-14 13:13:00 -0500

    Added full descriptions to resume

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,4 +24,36 @@ document.addEventListener('DOMContentLoaded', function() {
 			setTheme(theme === 'dark' ? 'light' : 'dark', true);
 		});
 	}
+
+	const detailsButton = document.getElementById('js-details-button');
+
+	if (detailsButton) {
+		const controlsIds = detailsButton.getAttribute('aria-controls').split(' ');
+		let detailsVisible = false;
+
+		detailsButton.addEventListener('click', function(event) {
+			event.preventDefault();
+
+			const controls = controlsIds.map(function(id) {
+				return document.getElementById(id);
+			});
+
+			controls.forEach(function(control) {
+				if (detailsVisible) {
+					control.hidden = true;
+					control.setAttribute('aria-expanded', false);
+				}
+				else {
+					control.hidden = false;
+					control.setAttribute('aria-expanded', true);
+				}
+			});
+
+			detailsButton.innerText = detailsVisible
+				? 'Show Details'
+				: 'Hide Details';
+
+			detailsVisible = !detailsVisible;
+		});
+	}
 });
