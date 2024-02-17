8a51809148146f13969f84cbb35bb2f3ae0b8171
Phil Buchanan <info@philbuchanan.com>
2024-02-17 17:49:19 -0500

    Improved theme switcher code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,27 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
 	'use strict';
+
+	const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+	let theme = localStorage.getItem('theme');
+
+	const setTheme = (toSet) => {
+		theme = toSet;
+		document.documentElement.setAttribute('data-theme', theme);
+	};
+
+	const saveTheme = (toSave) => {
+		localStorage.setItem('theme', toSave);
+	}
+
+	if ((!theme && darkModeQuery.matches) || theme === 'dark') {
+		setTheme('dark');
+	}
 
 	const themeSwitcherBtn = document.getElementById('js-theme-switcher-button');
 
 	if (themeSwitcherBtn) {
-		const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-		let theme = localStorage.getItem('theme');
-
-		const setTheme = function(newTheme, save) {
-			theme = newTheme;
-			document.documentElement.setAttribute('data-theme', theme);
-
-			if (save) {
-				localStorage.setItem('theme', theme);
-			}
-		};
-
-		if ((!theme && darkModeQuery.matches) || theme === 'dark') {
-			setTheme('dark', false);
-		}
-
 		themeSwitcherBtn.addEventListener('click', function() {
-			setTheme(theme === 'dark' ? 'light' : 'dark', true);
+			// eslint-disable-next-line no-console
+			const newTheme = theme === 'dark' ? 'light' : 'dark';
+			console.log(newTheme);
+			setTheme(newTheme);
+			saveTheme(newTheme);
 		});
 	}
 });
