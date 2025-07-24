79ad939824ac56da0f52165842fb2b2f243184db
Phil Buchanan <phil@philbuchanan.com>
2025-07-24 19:47:53 -0400

    Convert tabs to spaces

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-	'use strict';
+  'use strict';
 
-	const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
-	let theme = localStorage.getItem('theme');
+  const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  let theme = localStorage.getItem('theme');
 
-	const setTheme = (themeToSet) => {
-		theme = themeToSet;
-		document.documentElement.setAttribute('data-theme', theme);
-	};
+  const setTheme = (themeToSet) => {
+    theme = themeToSet;
+    document.documentElement.setAttribute('data-theme', theme);
+  };
 
-	const saveTheme = (themeToSave) => {
-		localStorage.setItem('theme', themeToSave);
-	}
+  const saveTheme = (themeToSave) => {
+    localStorage.setItem('theme', themeToSave);
+  }
 
-	if ((!theme && darkModeQuery.matches) || theme === 'dark') {
-		setTheme('dark');
-	}
+  if ((!theme && darkModeQuery.matches) || theme === 'dark') {
+    setTheme('dark');
+  }
 
-	const themeSwitcherBtn = document.getElementById('js-theme-switcher-button');
+  const themeSwitcherBtn = document.getElementById('js-theme-switcher-button');
 
-	if (themeSwitcherBtn) {
-		themeSwitcherBtn.addEventListener('click', function() {
-			const newTheme = theme === 'dark' ? 'light' : 'dark';
-			setTheme(newTheme);
-			saveTheme(newTheme);
-		});
-	}
+  if (themeSwitcherBtn) {
+    themeSwitcherBtn.addEventListener('click', function() {
+      const newTheme = theme === 'dark' ? 'light' : 'dark';
+      setTheme(newTheme);
+      saveTheme(newTheme);
+    });
+  }
 });
